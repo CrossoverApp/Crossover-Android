3f447daae02e5071fd83a28f62439955e3bdc809
Jeffrey <jeguo@ucsc.edu>
2016-02-08 10:39:21 -0800

    Still need ParseConstant, hamburger menu is generating dynamically now.

--- app/src/main/java/com/gmail/nelsonr462/crossover/MainActivity.java
+++ app/src/main/java/com/gmail/nelsonr462/crossover/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,23 +46,42 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        // *** PARSE LOGIN CHECK *** //
+        mCurrentUser = ParseUser.getCurrentUser();
+        if (mCurrentUser == null) {
+            navigateToLogin();
+        } else {
+            Snackbar.make(findViewById(R.id.drawer_layout), "Logged in!", Snackbar.LENGTH_LONG).show();
+        }
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        final Menu drawerMenu = navigationView.getMenu();
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("TabGroup");
+        query.whereEqualTo("user", mCurrentUser);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> tabGroups, ParseException e) {
+                if (e == null) {
+                    for (ParseObject tabGroup : tabGroups) {
+                        drawerMenu.add(tabGroup.getString("title"));
+                    }
+                } else {
+                    //TODO
+                }
+            }
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-        // *** PARSE LOGIN CHECK *** //
-        mCurrentUser = ParseUser.getCurrentUser();
-        if(mCurrentUser == null) {
-            navigateToLogin();
-        } else {
-            Snackbar.make(findViewById(R.id.drawer_layout), "Logged in!", Snackbar.LENGTH_LONG).show();
-        }
     }
 
     @Override
@@ -99,19 +127,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -125,4 +153,5 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
 
     }
+
 }
